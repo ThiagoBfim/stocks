1b046cd7abc8f9f57a8d49d9a44af17f96922032
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-08-10 10:29:05 -0300

    First commit

---
+++ src/main/java/com/money/stocks/StocksApplication.java
@@ -0,0 +1,13 @@
+package com.money.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/money/stocks/StocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.money.stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
