4d5d973209cc04fec202ab059173e3c27b9ec90d
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-08-11 18:24:47 -0300

    feat: sonar - clean code

--- src/main/java/com/money/stocks/service/search/ConnectionUtils.java
+++ src/main/java/com/money/stocks/service/search/ConnectionUtils.java
@@ -3,48 +3,23 @@ package com.money.stocks.service.search;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
 
 public class ConnectionUtils {
 
     private static final String UA_PHONE = "Mozilla/5.0 (Linux; Android 4.3; Nexus 10 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Safari/537.36";
     private static final int TIME_OUT = 10 * 1000;
 
+    private ConnectionUtils() {
+        /*Noop*/
+    }
+
     public static Document getConnection(String url) throws IOException {
         return Jsoup.connect(url)
                 .userAgent(UA_PHONE)
                 .timeout(TIME_OUT)
                 .ignoreContentType(true)
-                .sslSocketFactory(socketFactory())
+                .ignoreHttpErrors(true)
                 .get();
-    }
-
-    private static SSLSocketFactory socketFactory() {
-        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-
-            public void checkClientTrusted(X509Certificate[] certs, String authType) {
-            }
-
-            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-            }
-        }};
-
-        try {
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-            return sslContext.getSocketFactory();
-        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-            throw new RuntimeException("Failed to create a SSL socket factory", e);
-        }
     }
 }
