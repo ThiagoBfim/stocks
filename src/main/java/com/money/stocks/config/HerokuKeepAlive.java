d10ee5313aa172dc54b86de35e48077f73c36145
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-08-12 10:56:30 -0300

    fix: Fix heroku keep alive to be a Spring bean

--- src/main/java/com/money/stocks/config/HerokuKeepAlive.java
+++ src/main/java/com/money/stocks/config/HerokuKeepAlive.java
@@ -2,10 +2,11 @@ package com.money.stocks.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
-
+@Configuration
 public class HerokuKeepAlive {
 
     private static final long MIN_15 = 15 * 60 * 1000L;
